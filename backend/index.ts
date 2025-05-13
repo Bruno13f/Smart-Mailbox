e2acae8db5ae739603f4acc4f02cd938468578f6
Diogo Guerra <63842251+diogoguerra03@users.noreply.github.com>
2025-05-13 17:23:36 +0100

    feat: POST /cartas

--- backend/index.ts
+++ backend/index.ts
@@ -2,8 +2,15 @@ const server = Bun.serve({
   port: 3000,
   fetch(req) {
     const url = new URL(req.url);
-    if (url.pathname === "/") {
+
+    // GET
+    if (req.method === "GET" && url.pathname === "/") {
       return new Response("Olá, mundo!", { status: 200 });
+    }
+
+    // POST
+    if (req.method === "POST" && url.pathname === "/cartas") {
+      return new Response("Cartas no correio", { status: 200 });
     }
 
     return new Response("Página não encontrada", { status: 404 });
