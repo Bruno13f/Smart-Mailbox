c65421405f1c5a91ebbbdc9982dbd27c9b7c711c
Bruno Ferreira <bruno13f@outlook.pt>
2025-05-13 16:34:56 +0100

    feat: temperature sensor

--- simulation/src/main.cpp
+++ simulation/src/main.cpp
@@ -1,31 +1,41 @@
 #include <Arduino.h>
 
-int ledPin = 13;               
-int inputPin = 2;              
-int pirState = LOW;             
-int val = 0;                    
+const float BETA = 3950;
+int led = 12;                // the pin that the LED is atteched to
+int sensor = 2;              // the pin that the sensor is atteched to
+int state = LOW;             // by default, no motion detected
+int val = 0;                 // variable to store the sensor status (value)
 
 void setup() {
-  pinMode(ledPin, OUTPUT);      
-  pinMode(inputPin, INPUT);     
-
-  Serial.begin(9600);
+  pinMode(led, OUTPUT);      // initalize LED as an output
+  pinMode(sensor, INPUT);    // initialize sensor as an input
+  Serial.begin(9600);        // initialize serial
 }
 
-void loop() {
-  Serial.println("Looping...");
-  val = digitalRead(inputPin);  
-  if (val == HIGH) {            
-    digitalWrite(ledPin, HIGH);  
-    if (pirState == LOW) {
-      Serial.println("Motion detected!");
-      pirState = HIGH;
+void loop(){
+  val = digitalRead(sensor);   // read sensor value
+  if (val == HIGH) {           // check if the sensor is HIGH
+    digitalWrite(led, HIGH);   // turn LED ON
+    delay(100);                // delay 100 milliseconds 
+    
+    if (state == LOW) {
+      Serial.println("Motion detected! Counting 2 seconds..."); 
+      state = HIGH;       // update variable state to HIGH
     }
-  } else {
-    digitalWrite(ledPin, LOW);
-    if (pirState == HIGH) {
-      Serial.println("Motion ended!");
-      pirState = LOW;
+  } 
+  else {
+      digitalWrite(led, LOW); // turn LED OFF
+      delay(200);             // delay 200 milliseconds 
+      
+      if (state == HIGH){
+        Serial.println("Motion stopped!");
+        state = LOW;       // update variable state to LOW
     }
   }
+
+  int analogValue = analogRead(A0);
+  float celsius = 1 / (log(1 / (1023. / analogValue - 1)) / BETA + 1.0 / 298.15) - 273.15;
+  String temp = (String) celsius;
+  Serial.println(temp);
+  delay(1000);
 }
